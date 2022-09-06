81dbd58ac17bda362b489f919309ea988c47e6d4
GenesisGab <78931170+GenesisGab@users.noreply.github.com>
2022-09-05 21:17:39 -0600

    Add files via upload

---
+++ js/componentes/menu.js
@@ -0,0 +1,33 @@
+export function menu (){
+    let root = document.querySelector("#root");
+  
+   //CREACION DE LOS DIV PARA EL PANEL
+    root.innerHTML = 
+    '<div id="headerBanner">'+
+            '<img class="lineas" src="./img/lineas.png">'+
+    '</div>'+
+
+        '<header>'+
+            '<div class="logoHeader">'+
+                '<img src="./img/logo.png">'+
+            '</div>'+
+    
+            '<ul class="nav">'+
+                '<li><a class="hijoOption">Menú</a></li>'+
+                '<li><a class="hijoOption">Menú</a></li>'+
+                '<li><a class="hijoOption">Menú</a></li>'+
+                '<li><a class="hijoOption">Menú</a></li>'+
+            '</ul>'+
+
+            '<div class="flecha">'+
+                '<a><img class="imgFlecha"></a>'+
+                '<a><img class="imgFlecha"></a>'+
+                '<a><img class="imgFlecha"></a>'+
+                '<a><img id="flechaH" class="imgFlecha" src="./img/flecha.png"></a>'+
+            '</div>'+
+            
+          '</header>'+
+          '<section id="menu"></section>';
+       
+}
+menu();
---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* CONEXIONES */
+
+import { menu } from "./componentes/menu.js";
+import { cuadrosBromat } from "./componentes/bromatologia.js";
+import { piramide } from "./componentes/piramide.js";
+import { estadistica } from "./componentes/estadistica.js";
+
+/* DESPLIEGUE DE DATOS BROMATOLOGÍA */
+import { item } from "./componentes/bromatologia.js";
+import {bromatologia} from "./data/bromatologia.js";
+
+let root = document.querySelector("#broma");
+
+
+function cargarGaleria(){
+
+    let galeria = document.querySelector("#galeria");
+
+    bromatologia.forEach(function(elemento,indice){
+
+        let div = document.createElement("div");
+        div.setAttribute("class","item");
+        div.innerHTML = item(indice);
+
+        galeria.appendChild(div);
+    })
+
+}
+
+cargarGaleria();
